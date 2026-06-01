8744944c731d3097035aa173597131a5abc3135f
johnlauermann <72218401+johnlauermann@users.noreply.github.com>
2026-06-01 14:33:55 -0400

    multiple regression

--- Lab_06.R
+++ Lab_06.R
@@ -1,10 +1,12 @@
-# Lab 6: Multiple linear regression
+
+# Lab 6: Multiple linear regression ---------------------------------------
 
 library(corrplot)     # for a correlation matrix
 library(dplyr)        # for data management
 library(olsrr)        # for stepwise model building
 library(tidyverse)    # dependency for tidycensus
 library(tidycensus)   # for Census API
+
 
 # set up with census API
 ## I'll once again use the poverty variable that I used prior assignments, plus a few others
@@ -39,19 +41,21 @@ data <- data %>%
             S1501_C02_013M,S1701_C03_046M, S2301_C04_001M))  # select() uses - to remove columns
 
 
-# Q1: plot & define a research question
-## In my case: What predicts higher poverty rates in US counties?
 
-## generate scatterplots
-### create a matrix for analysis
+# Q1: plot & define a research question -----------------------------------
+
+# In my case: What predicts higher poverty rates in US counties?
+
+# generate scatterplots
+## create a matrix for analysis
 matrix <- data %>% select(-c(GEOID, NAME))   # drops the variable we don't want to correlate
 matrix <- na.omit(matrix)                # removes rows with null values
 
-### use pairs() to create the visualization
+## use pairs() to create the visualization
 pairs(matrix, main = "Potential Predictors of Poverty")
 pairs(matrix, main = "Potential Predictors of Poverty", upper.panel = NULL) ## this removes the duplication
 
-### correlation tests
+## correlation tests
 cor.test(data$PovertyRate, data$Bachpct)
 cor.test(data$PovertyRate, data$Postgradpct)
 cor.test(data$PovertyRate, data$UnemploymentRate)
@@ -59,36 +63,36 @@ cor.test(data$PovertyRate, data$MedHHIncome)
 
 
 
-# Q2: define a regression model, interpret coefficients
-## define the basic mode
+# Q2: define a regression model, interpret coefficients -------------------
+
+# define the basic mode
 model_1 <- lm(PovertyRate ~ Bachpct + Postgradpct + UnemploymentRate, data = data)  # define the model
 summary(model_1)  # print results
 
-## add an interaction effect
+# add an interaction effect
 model_2 <- lm(PovertyRate ~ Bachpct + Postgradpct + UnemploymentRate + (Bachpct * UnemploymentRate), data = data)
 summary(model_2)
 
 
 
-# Q3 Use forward, backward, or stepwise modeling
-## see this for documentation https://cran.r-project.org/web/packages/olsrr/vignettes/variable_selection.html
+# Q3 Use forward, backward, or stepwise modeling --------------------------
 
-## a forward selection adds predictor variables one at a time 
+# see this for documentation https://cran.r-project.org/web/packages/olsrr/vignettes/variable_selection.html
+
+# a forward selection adds predictor variables one at a time 
 ## this version evaluates each iteration based on how the new variables change R2
 forward <- ols_step_forward_r2(model_2) ## generate and save the modeling
 forward  ## print the report
 plot(forward)  ## visualize the change in R2 from one step to the next
 
-## a backward selection starts with all variables, then removes them one by one
+# a backward selection starts with all variables, then removes them one by one
 backward <- ols_step_backward_r2(model_2)
 backward
 plot(backward)
 
-## a both selection (stepwise) adds and removes in all possible combinations
+# a both selection (stepwise) adds and removes in all possible combinations
 both <- ols_step_both_r2(model_2)
 both
 plot(both)
 
-## or this function will run the possibible indicators, in addition to R2
-ols_step_all_possible(model_2)
 
